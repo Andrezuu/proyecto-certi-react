c31543dcd7149a3beebca6881a97cda273f20bd6
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2025-06-01 17:42:36 -0400

    MapPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// en src/main.tsx o index.tsx
+import 'leaflet/dist/leaflet.css';
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
